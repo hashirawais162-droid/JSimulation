d8e1bd7d6ee29dce2869dc98becf295da3b82160
hashirawais162-droid <hashirawais162@gmail.com>
2026-06-03 23:04:54 -0700

    Refactor JosephusSim class implementation
    Refactor JosephusSim class to improve structure and readability.

--- JosephusSim.java
+++ JosephusSim.java
@@ -2,108 +2,106 @@ import java.util.*;
 import java.io.*;
 
 public class JosephusSim {
-private PersonNode first;  // start of circle
-private int size;          // number of people
-private int eliminationCount;
-private PersonNode current; // helps track position
-   public JosephusSim(String fileName) {
-      try {
-         // load names from the file in order, generating a singly linked list of PersonNodes
-         Scanner file = new Scanner(new File(fileName));
-         
-         // make the ring circular by attaching last node's next to front
-         PersonNode prev = null;
 
-      while (file.hasNextLine()) {
-       String name = file.nextLine();
-       PersonNode newNode = new PersonNode(name);
+    private PersonNode first;   // start of circle
+    private int size;           // number of people
+    private int eliminationCount;
+    private PersonNode current; // helps track position
 
-       if (first == null) {
-        first = newNode;
-       } else {
-        prev.next = newNode;
-       }
+    public JosephusSim(String fileName) {
+        try {
+            Scanner file = new Scanner(new File(fileName));
 
-       prev = newNode;
-       size++;
-   }
-         // generate, print, and save the random elimination count
+            PersonNode prev = null;
 
-      } catch (FileNotFoundException e) {
-         System.out.println("Something went wrong with " + fileName);
-      }
-      prev.next = first;
-      current = first;
-      Random rand = new Random();
-      eliminationCount = rand.nextInt(size / 2) + 1;
-      System.out.println("=== Elimination count is " + eliminationCount + " ===");
-   }
-   
-   // optional helper method for constructing the circle
-   private void add(String val) {
-   }
-   
-   public void eliminate() {
-      // count to the elimination count
-    if (size <= 1) return;
+            // build linked list
+            while (file.hasNextLine()) {
+                String name = file.nextLine();
+                PersonNode newNode = new PersonNode(name);
 
-    // move to node BEFORE the one to delete
-    for (int i = 1; i < eliminationCount - 1; i++) {
-        current = current.next;
+                if (first == null) {
+                    first = newNode;
+                } else {
+                    prev.next = newNode;
+                }
+
+                prev = newNode;
+                size++;
+            }
+
+            // make circular
+            if (prev != null) {
+                prev.next = first;
+            }
+
+            // set current pointer
+            current = first;
+
+            // generate elimination count (1 to size/2)
+            Random rand = new Random();
+            eliminationCount = rand.nextInt(size / 2) + 1;
+
+            System.out.println("=== Elimination count is " + eliminationCount + " ===");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Something went wrong with " + fileName);
+        }
     }
 
-    PersonNode toDelete = current.next;
+    public void eliminate() {
+        if (size <= 1) return;
 
-    System.out.println(toDelete.name + " eliminated!");
-
-    // remove node
-    current.next = toDelete.next;
-
-    // update first if needed
-    if (toDelete == first) {
-        first = toDelete.next;
-    }
-
-    // move current forward
-    current = current.next;
-
-    size--;
-}
-      // print who will be eliminated
-      
-      // eliminate the person and update "front" of the circle and size
-
-   }
-   
-   public boolean isOver() {
-      // check if there's only one person left in the circle
-    return size == 1;
-   }
-   
-   public String toString() {
-      // if there's only one person left, print them as the last survivor
-    if (size == 1) {
-        return first.name + " is the last survivor!";
-    }
-
-    String result = "Remaining survivors: ";
-
-    PersonNode temp = first;
-    int count = 1;
-
-    do {
-        result += count + "-" + temp.name;
-        temp = temp.next;
-        count++;
-
-        if (temp != first) {
-            result += ", ";
+        // move to node BEFORE the one to delete
+        for (int i = 1; i < eliminationCount - 1; i++) {
+            current = current.next;
         }
 
-    } while (temp != first);
+        PersonNode toDelete = current.next;
 
-    return result;
-}
-      // if many, print survivors (watch out for infinite loop since list is circular)
+        System.out.println(toDelete.name + " eliminated!");
 
+        // remove node
+        current.next = toDelete.next;
+
+        // update first if needed
+        if (toDelete == first) {
+            first = toDelete.next;
+        }
+
+        // move forward for next round
+        current = current.next;
+
+        size--;
+    }
+
+    public boolean isOver() {
+        return size == 1;
+    }
+
+    public String toString() {
+        if (size == 0) return "";
+
+        // last survivor
+        if (size == 1) {
+            return first.name + " is the last survivor!";
+        }
+
+        String result = "Remaining survivors: ";
+
+        PersonNode temp = first;
+        int count = 1;
+
+        do {
+            result += count + "-" + temp.name;
+            temp = temp.next;
+            count++;
+
+            if (temp != first) {
+                result += ", ";
+            }
+
+        } while (temp != first);
+
+        return result;
+    }
 }
